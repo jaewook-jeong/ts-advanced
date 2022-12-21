f9cba6172fa9fb622389c73e55a743246c517d6b
woogi <dnrlwo11@gmail.com>
2022-12-21 18:57:08 +0900

    03. 배운것정리

--- exercises/03-wrapExternalLib.code.ts
+++ exercises/03-wrapExternalLib.code.ts
@@ -9,3 +9,14 @@ export const fetchUserWithFullName = async (
     fullName: `${user.firstName} ${user.lastName}`,
   };
 };
+
+// Parameters 유틸리티 타입을 쓰면 해당 타입의 배열화 되네..
+// returntype은 리턴타입을 타입으로 만들어주네..
+
+type ExFunc = ({hello}: {hello: number }) => {chris: string};
+
+type Ptype = Parameters<ExFunc>;
+//[{hello: number}]
+
+type Rtype = ReturnType<ExFunc>;
+// {chris: string}
