5094063f0a415cc3da8565e8c1d39c9472a13472
woogi <dnrlwo11@gmail.com>
2022-12-21 18:57:02 +0900

    02. 배운것정리

--- exercises/02-roleBasedAccess.code.ts
+++ exercises/02-roleBasedAccess.code.ts
@@ -5,7 +5,9 @@ const userAccessModel = {
 } as const;
 
 export type Role = keyof typeof userAccessModel;
+// 이렇게 하면 배열인 키의 모든 값을 알 수 있다.
 export type Action = typeof userAccessModel[Role][number];
+
 
 export const canUserAccess = (role: Role, action: Action) => {
   return (userAccessModel[role] as ReadonlyArray<Action>).includes(action);
